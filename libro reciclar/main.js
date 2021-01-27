1fc500265725e0476262ef89a8e3f62d447447c8
unknown <sandrarehan97@gmail.com>
2021-01-27 13:26:10 +0530

    libro reciclar

---
+++ libro reciclar/main.js
@@ -0,0 +1,25 @@
+var feild = document.querySelector('textarea');
+var backUp = feild.getAttribute('placeholder');
+var btn = document.querySelector('.btn3');
+var clear = document.getElementById('clear')
+
+feild.onfocus = function(){
+    this.setAttribute('placeholder', '');
+    this.style.borderColor = '#333';
+    btn.style.display = 'block'
+}
+
+feild.onblur = function(){
+    this.setAttribute('placeholder',backUp);
+    this.style.borderColor = '#aaa'
+}
+
+clear.onclick = function(){
+    btn.style.display = 'none';
+    feild.value = '';
+}
+function jFunction(){
+    if(document.getElementById('get').checked){
+           document.write("Welcome");
+    }
+}
